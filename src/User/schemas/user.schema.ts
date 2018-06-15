217cbb0e9e3fa8a8a80878379d3a3d72c239a248
Casey Wang <caseywang@httpeace.com>
2018-06-15 18:46:28 +0800

    change user schema append roles

--- src/User/schemas/user.schema.ts
+++ src/User/schemas/user.schema.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
+  roles: { type: Array, default: ['user'] },
   createdAt: { type: Date, default: Date.now },
   password: { type: String, required: true },
 });
