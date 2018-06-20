81797240bbaeb624c27f425c42ee1b370e6a70a9
Casey Wang <caseywang@httpeace.com>
2018-06-20 09:54:27 +0800

    config graphql and create cat service empty folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { graphqlExpress } from 'apollo-server-express';
+import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { AuthModule } from './Auth/auth.module';
 import { UserModule } from './User/user.module';
+import { CatModule } from './Cat/cat.module';
 import { ArticleModule } from './Article/article.module';
 
 @Module({
   imports: [
+    GraphQLModule,
     MongooseModule.forRoot('mongodb://localhost/support'),
-    ArticleModule,
     AuthModule,
     UserModule,
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(private readonly graphQLFactory: GraphQLFactory) {}
+  configure(comsumer: MiddlewareConsumer) {
+    // const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');
+    // const schema = this.graphQLFactory.createSchema({ typeDefs });
+    // comsumer
+    // .apply(graphqlExpress(req => ({ schema, rootValue: req })))
+    // .forRoutes('/graphql');
+  }
+}
