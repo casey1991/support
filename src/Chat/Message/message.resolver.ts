e3ae9e870c83edd0ca742fa32ab14fe90cfa9acc
Casey Wang <caseywang@httpeace.com>
2018-09-09 15:58:50 +0800

    create createMessage

--- src/Chat/Message/message.resolver.ts
+++ src/Chat/Message/message.resolver.ts
@@ -4,10 +4,13 @@ import {
   ResolveProperty,
   Args,
   Parent,
+  Mutation,
 } from '@nestjs/graphql';
 import { MessageService } from './message.service';
 import { UserService } from '../../User/user.service';
 import { RoomService } from '../Room/room.service';
+
+import { MessageCreateDto } from './dto/message.create.dto';
 
 import { find } from 'lodash';
 @Resolver('Message')
@@ -34,5 +37,20 @@ export class MessageResolver {
   async getUser(@Parent() message) {
     const userId = message.user;
     return await this.userService.findUser(userId);
+  }
+  @Mutation()
+  async createMessage(
+    @Args('roomId') roomId: string,
+    @Args('userId') userId: string,
+    @Args('type') type: number,
+    @Args('text') text: string,
+  ) {
+    const dto = new MessageCreateDto();
+    dto.room = roomId;
+    dto.user = userId;
+    dto.text = text;
+    dto.type = type;
+    const message = await this.messageService.createMessage(dto);
+    return message;
   }
 }
