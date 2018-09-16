dc6d34854effc25c87aa22c448b459e891cd7d0f
Casey Wang <caseywang@httpeace.com>
2018-09-16 19:33:15 +0800

    add file fileds in goods & shop

--- src/Goods/schemas/goods.schema.ts
+++ src/Goods/schemas/goods.schema.ts
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 export const GoodsSchema = new mongoose.Schema({
   name: { type: Schema.Types.String, required: true },
   price: { type: Schema.Types.Number, required: true },
+  images: [{ type: Schema.Types.ObjectId, ref: 'File' }],
   amount: { type: Schema.Types.Number, required: true },
   type: { type: Schema.Types.Number, default: 0 }, // type: maybe private or shop? 0 for private 1 for shop
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
--- src/Shop/schemas/shop.schema.ts
+++ src/Shop/schemas/shop.schema.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 export const ShopSchema = new mongoose.Schema({
   name: { type: Schema.Types.String, required: true },
+  images: [{ type: Schema.Types.ObjectId, ref: 'File' }],
   owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   goods: [{ type: Schema.Types.ObjectId, ref: 'Goods' }],
 });
